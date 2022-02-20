935e948b702a4ccbe6a96c30c4d23b3fd0b9f48a
David Sn <divad.nnamtdeis@gmail.com>
2022-02-20 21:30:00 +0100

    Switch to HLS for playback and enable DVR to 30 minutes
    Signed-off-by: David Sn <divad.nnamtdeis@gmail.com>

--- wwwdata/js/player.js
+++ wwwdata/js/player.js
@@ -1,4 +1,4 @@
-var player = videojs("player");
+var player = videojs("player", {liveui: true});
 
 player.ready(function() {
     let params = new URLSearchParams(window.location.search);
@@ -6,13 +6,13 @@ player.ready(function() {
 
     if (stream) {
         player.src({
-            src: "/live/" + stream + ".mpd",
-            type: "application/dash+xml"
+            src: "/live/" + stream + "/index.m3u8",
+            type: "application/vnd.apple.mpegurl"
         });
     } else {
         player.src({
-            src: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
-            type: "video/mp4"
+            src: "https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8",
+            type: "application/vnd.apple.mpegurl"
         });
     }
 });
