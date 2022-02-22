e66fb9fac026893b7f4cecfc48641cf5d9daee99
David Sn <divad.nnamtdeis@gmail.com>
2022-02-22 17:21:13 +0100

    Use ffmpeg to generate a livestream preview every minute
    Signed-off-by: David Sn <divad.nnamtdeis@gmail.com>

--- wwwdata/js/player.js
+++ wwwdata/js/player.js
@@ -5,11 +5,13 @@ player.ready(function() {
     let stream = params.get("stream");
 
     if (stream) {
+        player.poster("/live/" + stream + "/preview.jpg");
         player.src({
             src: "/live/" + stream + "/index.m3u8",
             type: "application/vnd.apple.mpegurl"
         });
     } else {
+        player.poster("https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/PM5644.svg/720px-PM5644.svg.png");
         player.src({
             src: "https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8",
             type: "application/vnd.apple.mpegurl"
