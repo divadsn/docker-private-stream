46d57ba6e2e47eda6c5d830f0d3000a0fe323b3e
David Sn <divad.nnamtdeis@gmail.com>
2022-03-09 16:30:08 +0100

    Add index.m3u8 as index for /live location
    Signed-off-by: David Sn <divad.nnamtdeis@gmail.com>

--- wwwdata/js/player.js
+++ wwwdata/js/player.js
@@ -7,13 +7,7 @@ player.ready(function() {
     if (stream) {
         player.poster("/live/" + stream + "/preview.jpg");
         player.src({
-            src: "/live/" + stream + "/index.m3u8",
-            type: "application/vnd.apple.mpegurl"
-        });
-    } else {
-        player.poster("https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/PM5644.svg/720px-PM5644.svg.png");
-        player.src({
-            src: "https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8",
+            src: "/live/" + stream + "/",
             type: "application/vnd.apple.mpegurl"
         });
     }
